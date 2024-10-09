17a5886f4cf3ce3ac2ee1733646205f32d7776b6
Lucas de Melo <lm159753456@gmail.com>
2024-10-09 13:56:58 -0300

    mudança em calculo numerico

--- calculo_numerico/zero.py
+++ calculo_numerico/zero.py
@@ -1,22 +1,30 @@
 
+import math
+
+#def funcao(x):
+#    return x**3+x-8
+
 def funcao(x):
-    return x - (2**-x)
+    return 1-x*math.log(x)
 
 def calcularZero(a, b, tol):
+    #iterações
+    i=0
     #teorema de Bolzano
     if(funcao(a)*funcao(b) < 0):
-
         while(True): #-> os "return" quebram o laço while quando estiver dentro da tolerancia escolhida
             refinamento = (a + b) / 2
             refinamento_aplicado = funcao(refinamento)
+            i += 1
 
             #verifica se em algum dos eixos o refinamento provocou um resultado dentro do
             #intervalo de tolerancia
             tolY = abs(refinamento_aplicado) < tol #numero absoluto - modulo
-            tolX = ((b - a) / 2) < tol
+            tolX = abs((b - a) / 2) < tol
 
             #programação defensiva
             if(refinamento_aplicado == 0 or tolY or tolX):
+                print(f"Número de iterações: {i}")
                 return refinamento
             #+++
             elif(refinamento_aplicado > 0):
@@ -32,21 +40,19 @@ def calcularZero(a, b, tol):
                     a = refinamento
                 else:
                     b = refinamento
-
+    else:  
+        return("(ERROR : Intervalo não satisfaz o teorema de bolzano)")
+            
 
 #intervalo inicial
-a = 0  #intervalo da esquerda
-b = 2  #intervalo da direita
-
-a = int(input("Digite o intervalo esquerdo:"))
-b = int(input("Digite o intervalo direito:"))
+a = float(input("Digite o intervalo esquerdo:")) #inserir decimal com ponto ex 0.5
+b = float(input("Digite o intervalo direito:"))
 
 #precisão
-tol = 0.01
-
 tol = float(input("Digite a tolerancia de erro desejada: "))
 
 #resultado
 x = calcularZero(a, b, tol)
-print(x)
 
+#impressão
+print(f"No intervalo [{a}, {b}] com precisão de {tol} a raiz será {x}")
